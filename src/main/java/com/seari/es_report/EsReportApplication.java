e5131aca6bdc1c1d815c1acdc54001fa53fef8ea
huangyixiang <yixiangh163@163.com>
2020-02-15 15:49:32 +0800

    Initial commit

---
+++ src/main/java/com/seari/es_report/EsReportApplication.java
@@ -0,0 +1,13 @@
+package com.seari.es_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seari/es_report/EsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seari.es_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
